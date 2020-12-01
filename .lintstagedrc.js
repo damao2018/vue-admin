dc00d6a1392dd388eef344ca9c27ede95df41677
jkkongkong <328346913@qq.com>
2020-12-01 15:39:02 +0800

    First commit

---
+++ .lintstagedrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+
+      "src/**/\*{js,jsx,ts,tsx,md,html}": ["eslint", "prettier --write"],
+  
+       "src/**/*.scss": ["stylelint --fix", "git add"],
+  
+  }
---
+++ src/main.ts
@@ -0,0 +1,32 @@
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+
+createApp(App)
+  .use(store)
+  .use(router)
+  .mount('#app');
+const test = 123;
+console.log(test);
+if (test) {
+  console.log(1);
+} else {
+  console.log(2);
+}
+const hah = (tex: number) => {
+  console.log(tex);
+};
+hah(123);
+const userInfo = {
+  code: 200,
+  id: '123456',
+  msg: 'success',
+  name: 'gs',
+  tel: '12306',
+};
+const  user={
+...userInfo,
+};
+
+console.log(user);
